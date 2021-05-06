f4082149161d0ff60e6336162f229a24a30be4f3
unknown <vladyslavvasylenko03@gmail.com>
2021-05-06 16:46:17 +0300

    fixes

--- js/script.js
+++ js/script.js
@@ -10,3 +10,18 @@ $(document).ready(function(){
     arrows: false,
   });
 });
+
+const anchors = document.querySelectorAll('a.scroll-to')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href')
+    
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
